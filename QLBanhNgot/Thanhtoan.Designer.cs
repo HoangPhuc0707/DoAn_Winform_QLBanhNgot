80a0561551d06c03836401a1b05f24957c6ba8ea
Phuc258749 <nguyenhoangphuc07072002@gmail.com>
2022-10-12 10:00:26 +0700

    test

--- QLBanhNgot/Thanhtoan.Designer.cs
+++ QLBanhNgot/Thanhtoan.Designer.cs
@@ -40,7 +40,6 @@
             this.Soluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
@@ -132,7 +131,7 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(651, 355);
+            this.dataGridView1.Size = new System.Drawing.Size(796, 355);
             this.dataGridView1.TabIndex = 2;
             // 
             // tenbanh
@@ -162,7 +161,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 147);
+            this.label2.Location = new System.Drawing.Point(34, 150);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(117, 16);
             this.label2.TabIndex = 3;
@@ -171,20 +170,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(862, 275);
+            this.label3.Location = new System.Drawing.Point(862, 337);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "TỔNG TIỀN";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(794, 572);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
             // 
             // textBox1
             // 
@@ -194,7 +184,7 @@
             this.textBox1.BackColor = System.Drawing.Color.Pink;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(948, 265);
+            this.textBox1.Location = new System.Drawing.Point(942, 323);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(240, 30);
@@ -204,7 +194,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(871, 313);
+            this.button2.Location = new System.Drawing.Point(865, 417);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(317, 23);
             this.button2.TabIndex = 6;
@@ -219,7 +209,6 @@
             this.ClientSize = new System.Drawing.Size(1254, 634);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
@@ -251,7 +240,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Soluong;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
     }
